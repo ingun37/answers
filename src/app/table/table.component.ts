59a60cbfc6874f9c2f2cf39f7b8c82916569571a
Ingun Jon <ingun37@gmail.com>
2019-11-12 18:20:21 +0900

    routing

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./table.component.sass']
 })
 export class TableComponent implements OnInit {
+  @Input() path: string;
   @Input() item;
   constructor() { }
 
