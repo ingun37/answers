f5085eda1dec55476f960532a3656c673fc5dbab
Ingun Jon <ingun37@gmail.com>
2019-11-28 01:30:34 +0900

    finally math parsing

--- src/app/md/md.component.ts
+++ src/app/md/md.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AnswerService, Item } from '../answer.service';
-import * as markit from "markdown-it";
+import * as markit from 'markdown-it';
 
 @Component({
   selector: 'app-md',
   templateUrl: './md.component.html',
   styleUrls: ['./md.component.sass']
 })
+
+
 export class MdComponent implements OnInit {
   @Input() item: Item;
   questionHTML: string;
@@ -16,10 +18,27 @@ export class MdComponent implements OnInit {
     private answerService: AnswerService
   ) { }
 
+
   ngOnInit() {
     this.markdownit = new markit();
-    this.questionHTML = this.markdownit.render(this.item.questionMD);
-    this.answerHTML = this.markdownit.render(this.item.answerMD);
+    const parser = new DOMParser();
+    this.questionHTML = latex(parser, this.markdownit.render(this.item.questionMD));
+    this.answerHTML = latex(parser, this.markdownit.render(this.item.answerMD));
   }
 
+}
+
+function latex(parser: DOMParser, htmlText: string): string {
+  if (!htmlText) {
+    return '';
+  }
+  const doc = parser.parseFromString(htmlText, 'text/html');
+  const aaa = doc.getElementsByTagName('*');
+  // tslint:disable-next-line: prefer-for-of
+  for (let index = 0; index < aaa.length; index++) {
+    const element = aaa[index];
+    element.textContent = element.textContent.replace(/\$.+?\$/g, '  -math-  ');
+  }
+  console.log(doc.documentElement.outerHTML);
+  return doc.documentElement.outerHTML;
 }
