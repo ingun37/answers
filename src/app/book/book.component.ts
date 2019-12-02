374371e35107cc0f2c08d3d556f7be33dcb2725f
Ingun Jon <ingun37@gmail.com>
2019-12-02 23:46:59 +0900

    section md

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item, AnswerService } from '../answer.service';
 import { ActivatedRoute } from '@angular/router';
-import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
-import { MarkdownService } from 'ngx-markdown';
-
-declare var katex: any;
 
 @Component({
   selector: 'app-book',
@@ -13,14 +9,10 @@ declare var katex: any;
 })
 export class BookComponent implements OnInit {
   item: Item;
-  questionHTML: SafeHtml;
-  answerHTML: SafeHtml;
 
   constructor(
     private route: ActivatedRoute,
     private answers: AnswerService,
-    private sanitizer: DomSanitizer,
-    private markdownService: MarkdownService
     ) { }
 
   ngOnInit() {
@@ -28,48 +20,8 @@ export class BookComponent implements OnInit {
       const bookId = params.get('id');
       this.answers.getItem('books/' + bookId).then(item => {
         this.item = item;
-        this.questionHTML = this.sanitizer.bypassSecurityTrustHtml(md2HTML(this.markdownService, item.questionMD));
-        this.answerHTML = this.sanitizer.bypassSecurityTrustHtml(md2HTML(this.markdownService, item.answerMD));
       });
     });
   }
 
-}
-
-function md2HTML(mdservice: MarkdownService, mdStr: string): string {
-  var displayTable = {};
-  var table = {};
-  const replacedMD = mdStr.replace(/\$\$(.+?)\$\$/gm, (match, group) => {
-    const randomID = makeid(16);
-    displayTable[randomID] = group;
-    return randomID;
-  }).replace(/\$(.+?)\$/g, (match, group) => {
-    const randomID = makeid(16);
-    table[randomID] = group;
-    return randomID;
-  });
-  var html = mdservice.compile(replacedMD);
-  for (const key in displayTable) {
-    if (displayTable.hasOwnProperty(key)) {
-      const tex = displayTable[key];
-      html = html.replace(key, katex.renderToString(tex, {displayMode: true}));
-    }
-  }
-  for (const key in table) {
-    if (table.hasOwnProperty(key)) {
-      const tex = table[key];
-      html = html.replace(key, katex.renderToString(tex));
-    }
-  }
-  return html;
-}
-
-function makeid(length) {
-  let result           = '';
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
 }
--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../answer.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { MarkdownService } from 'ngx-markdown';
+
+declare var katex: any;
 
 @Component({
   selector: 'app-section',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section.component.sass']
 })
 export class SectionComponent implements OnInit {
+  @Input() item: Item;
+  questionHTML: SafeHtml;
+  answerHTML: SafeHtml;
 
-  constructor() { }
+  constructor(
+    private sanitizer: DomSanitizer,
+    private markdownService: MarkdownService
+    ) { }
 
   ngOnInit() {
+    this.questionHTML = this.sanitizer.bypassSecurityTrustHtml(md2HTML(this.markdownService, this.item.questionMD));
+    this.answerHTML = this.sanitizer.bypassSecurityTrustHtml(md2HTML(this.markdownService, this.item.answerMD));
   }
+}
 
+function md2HTML(mdservice: MarkdownService, mdStr: string): string {
+  var displayTable = {};
+  var table = {};
+  const replacedMD = mdStr.replace(/\$\$(.+?)\$\$/gm, (match, group) => {
+    const randomID = makeid(16);
+    displayTable[randomID] = group;
+    return randomID;
+  }).replace(/\$(.+?)\$/g, (match, group) => {
+    const randomID = makeid(16);
+    table[randomID] = group;
+    return randomID;
+  });
+  var html = mdservice.compile(replacedMD);
+  for (const key in displayTable) {
+    if (displayTable.hasOwnProperty(key)) {
+      const tex = displayTable[key];
+      html = html.replace(key, katex.renderToString(tex, {displayMode: true}));
+    }
+  }
+  for (const key in table) {
+    if (table.hasOwnProperty(key)) {
+      const tex = table[key];
+      html = html.replace(key, katex.renderToString(tex));
+    }
+  }
+  return html;
+}
+
+function makeid(length) {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
