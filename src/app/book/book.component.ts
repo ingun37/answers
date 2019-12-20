522c72a04b64c09e4d1dd67dab0795dc81ed271c
Ingun Jon <ingun37@gmail.com>
2019-12-21 03:29:58 +0900

    tool bar for book

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { _Node, StaticDBService, _Item } from '../static-db.service';
+import { ClipboardService } from 'ngx-clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ClipboardSnackbarComponent } from '../clipboard-snackbar/clipboard-snackbar.component';
 
 @Component({
   selector: 'app-book',
@@ -15,8 +18,17 @@ export class BookComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private db: StaticDBService,
-    ) { }
+    private clipboard: ClipboardService,
+    private _snackBar: MatSnackBar
 
+  ) { }
+
+  shareClick(item: _Item) {
+    this.clipboard.copyFromContent(location.origin + '/books/' + item.sha1);
+    this._snackBar.openFromComponent(ClipboardSnackbarComponent, {
+      duration: 4 * 1000,
+    });
+  }
   change(event: MatButtonToggleChange) {
   }
   ngOnInit() {
