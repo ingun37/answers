b133f837b4fa18038f36930e4ad3306146f3b214
Ingun <ingun37@gmail.com>
2020-08-22 02:31:29 +0900

    dual space

--- gen-static-db.sh
+++ gen-static-db.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
+#--file-descriptor-count
+
+ulimit -n 512
 /nix/store/j2afrzqzwglsjhin0z7d8ydx0fp3l6s6-json-generator-0.1.0.0/bin/json-generator ../answers-db/books/ src/assets/db/
