3309115536f9e0f23ac368feab04b067b81fcb6c
Ingun Jon <ingun37@gmail.com>
2020-10-18 11:00:20 +0900

    remove dist

--- json-generator/test/Test.hs
+++ json-generator/test/Test.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import Lib (parse)
-import System.Directory (listDirectory)
+import System.Directory (listDirectory, removeDirectoryRecursive, createDirectoryIfMissing)
 
 main :: IO ()
 main = do
-  listDir
+  removeDirectoryRecursive "test/data/to"
+  createDirectoryIfMissing True "test/data/to/db"
+  createDirectoryIfMissing True "test/data/to/assets"
   parse ["test/data/from", "test/data/to/db"]
