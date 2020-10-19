3c7e474bd2299f9852f3eeae122b03b1750cce38
Ingun Jon <ingun37@gmail.com>
2020-10-19 21:41:48 +0900

    use latest script

--- gen-db/a.nix
+++ gen-db/a.nix
@@ -1,6 +1,6 @@
 let git = builtins.fetchGit {
         url = https://github.com/ingun37/haskell-script.git;
-        rev = "2e194a47791aa1d562d0b2c80c02d7f3238286e7";
+        rev = "fd66558bd6cbf8e3a793a530d6ebf56497d3dd81";
     };
     haskellScriptD = import git {};
 in  derivation {
