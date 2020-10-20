01f70009eed09d7f31b01c810172f42c93824d0d
Ingun Jon <ingun37@gmail.com>
2020-10-20 21:53:51 +0900

    better nix script

--- gen-db/gen-db.sh
+++ gen-db/gen-db.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 BASEDIR=$(dirname "$0")
-nix-shell "$BASEDIR/a.nix" --run "\$script/bin/json-generator $1 $BASEDIR/../src/assets"
+nix-shell "$BASEDIR/a.nix" --run "json-generator $1 $BASEDIR/../src/assets"
