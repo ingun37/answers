eb3e9be76a2c8bddebe0dc1257f12df6eb67013c
Ingun Jon <ingun37@gmail.com>
2022-03-13 23:34:17 +0900

    remove unused

--- compile-proto.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-protoc -I=../answers-proto dash.proto \
---js_out=import_style=commonjs,binary:src/app \
---grpc-web_out=import_style=typescript,mode=grpcwebtext:src/app
--- gen-db/gen-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-BASEDIR=$(dirname "$0")
-nix-shell "$BASEDIR/a.nix" --run "json-generator $1 $BASEDIR/../src/assets"
