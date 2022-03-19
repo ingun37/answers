00ac3fdc2bd5a191623091f952f232f0bc952f05
Ingun Jon <ingun37@gmail.com>
2022-03-19 15:43:48 +0900

    share icon

--- src/pages/recursive.tsx
+++ src/pages/recursive.tsx
@@ -9,37 +9,80 @@ import { useEffect, useState } from "react";
 import { either } from "fp-ts";
 import MathMD from "./mathmd";
 import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
 
+import ShareIcon from "@mui/icons-material/Share";
+type State =
+  | {
+      type: "loading";
+    }
+  | {
+      type: "treeTem";
+      treeTem: TreeTemT;
+    }
+  | {
+      type: "error";
+      msg: string;
+    };
 export default function Recursive(props: { sha1: string }) {
-  const [treeTem, setTreeTem] = useState<TreeTemT | null>(null);
+  const [state, setState] = useState<State>({ type: "loading" });
+
   useEffect(() => {
     fetch("/db/" + props.sha1 + ".json")
       .then((x) => x.json())
       .then(TreeTem.decode)
-      .then(either.match(console.error, setTreeTem))
-      .catch(console.error);
+      .then(
+        either.match(
+          (e) => {
+            setState({
+              type: "error",
+              msg: "Decoding failed: \n" + JSON.stringify(e, undefined, 2),
+            });
+          },
+          (x) => setState({ type: "treeTem", treeTem: x })
+        )
+      )
+      .catch((e) => setState({ type: "error", msg: e.toString() }));
   }, []);
-  return treeTem === null ? (
-    <div>loading</div>
-  ) : (
-    <div>
-      {treeTem.item.attr.q && <MathMD htmlString={treeTem.item.attr.q} />}
-      <Divider variant="middle" />
-      {treeTem.item.attr.a && <MathMD htmlString={treeTem.item.attr.a} />}
-      {treeTem.kids.map((item) => (
-        <Accordion TransitionProps={{ unmountOnExit: true }} key={item.sha1}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>{item.title}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Recursive sha1={item.sha1} />
-          </AccordionDetails>
-        </Accordion>
-      ))}
-    </div>
-  );
+  switch (state.type) {
+    case "loading":
+      return <div>loading</div>;
+    case "error":
+      return <div>{state.msg}</div>;
+    case "treeTem":
+      const { treeTem } = state;
+      return (
+        <div>
+          {treeTem.item.attr.q && <MathMD htmlString={treeTem.item.attr.q} />}
+          <Divider variant="middle" />
+          {treeTem.item.attr.a && <MathMD htmlString={treeTem.item.attr.a} />}
+          {treeTem.kids.map((item) => (
+            <Accordion
+              TransitionProps={{ unmountOnExit: true }}
+              key={item.sha1}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography sx={{ flex: 2 }}>{item.title}</Typography>
+                <IconButton
+                  color="primary"
+                  component="span"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <ShareIcon />
+                </IconButton>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Recursive sha1={item.sha1} />
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </div>
+      );
+  }
 }
