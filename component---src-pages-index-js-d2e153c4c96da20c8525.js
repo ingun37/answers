9e76743b11d044f6e7c04a7fd0d6e07d64ac7042
ingun37 <ingun37@users.noreply.github.com>
2022-03-20 04:41:16 +0000

    deploy: 249ba5de834b5579ab47805745d3f923107f6bee

---
+++ component---src-pages-index-js-d2e153c4c96da20c8525.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmath_book_answers=self.webpackChunkmath_book_answers||[]).push([[678],{6558:function(e,t,s){s.r(t);var a=s(7294),n=s(814),l={color:"#232129",fontFamily:"-apple-system, Roboto, sans-serif, serif"};t.default=function(){return a.createElement("main",{style:l},a.createElement(n.default,null))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d2e153c4c96da20c8525.js.map
