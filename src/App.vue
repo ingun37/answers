3303d758c6e6483437b9a7af7dc5ba72048a60af
ingun <ingun37@gmail.com>
2025-09-11 20:19:47 +0900

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app>
+    <v-app-bar color="primary" density="comfortable" flat>
+      <v-btn
+        icon="mdi-arrow-left"
+        variant="text"
+        @click="goBack"
+        aria-label="Go back"
+      />
+      <v-toolbar-title>App</v-toolbar-title>
+    </v-app-bar>
+
     <v-main>
       <router-view />
     </v-main>
@@ -7,5 +17,5 @@
 </template>
 
 <script lang="ts" setup>
-  //
+function goBack() {}
 </script>
